0091fbc10f70439d9f6d4487a631d9aba6207114
Vines <dv4633833@gmail.com>
2026-08-22 18:29:06 +0700

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,10 @@ plugins {
     alias(libs.plugins.fabric.loom)
 }
 
-val archivesBaseName = providers.gradleProperty("archives_base_name").get()
-val mavenGroup = providers.gradleProperty("maven_group").get()
-
 base {
-    archivesName = archivesBaseName
+    archivesName = properties["archives_base_name"] as String
     version = libs.versions.mod.version.get()
-    group = mavenGroup
+    group = properties["maven_group"] as String
 }
 
 repositories {
@@ -16,6 +13,7 @@ repositories {
         name = "meteor-maven"
         url = uri("https://maven.meteordev.org/releases")
     }
+
     maven {
         name = "meteor-maven-snapshots"
         url = uri("https://maven.meteordev.org/snapshots")
@@ -25,16 +23,50 @@ repositories {
 dependencies {
     // Fabric
     minecraft(libs.minecraft)
-    implementation(libs.fabric.loader)
+    mappings(variantOf(libs.yarn) { classifier("v2") })
+    modImplementation(libs.fabric.loader)
 
     // Meteor
-    implementation(libs.meteor.client)
+    modImplementation(libs.meteor.client)
 }
 
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(libs.versions.jdk.get().toInt()))
+tasks {
+    processResources {
+        val propertyMap = mapOf(
+            "version" to project.version,
+            "mc_version" to libs.versions.minecraft.get()
+        )
+
+        inputs.properties(propertyMap)
+
+        filteringCharset = "UTF-8"
+
+        filesMatching("fabric.mod.json") {
+            expand(propertyMap)
+        }
     }
+
+    jar {
+        inputs.property("archivesName", project.base.archivesName.get())
+
+        from("LICENSE") {
+            rename { "${it}_${inputs.properties["archivesName"]}" }
+        }
+    }
+
+    java {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+    withType<JavaCompile> {
+        options.encoding = "UTF-8"
+        options.release = 21
+
+        options.compilerArgs.add("-Xlint:deprecation")
+        options.compilerArgs.add("-Xlint:unchecked")
+    }
+}    }
 }
 
 fun toMinecraftCompat(version: String): String {
